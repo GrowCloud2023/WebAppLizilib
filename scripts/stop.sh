3fecd5d6e698aaae8625dad6307cfc5565d114ea
Felipe Bastidas <felipebastidas2013@gmail.com>
2023-07-20 21:37:26 -0400

    Cambio stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,6 +3,7 @@
 # Busca el ID del proceso de npm
 npm_process_id=$(ps aux | grep '[n]pm' | awk '{print $2}')
 
+
 if [ -n "$npm_process_id" ]; then
   # Detiene el proceso de npm
   echo "Deteniendo el proceso de npm con ID: $npm_process_id"
